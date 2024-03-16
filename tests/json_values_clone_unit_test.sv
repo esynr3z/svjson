77b6b88a7b213db4a1fe1f5942d761aae733a75b
esynr3z <esynr3z@gmail.com>
2024-03-16 15:25:12 +0300

    test: add more unit tests

---
+++ tests/json_values_clone_unit_test.sv
@@ -0,0 +1,32 @@
+`include "svunit_defines.svh"
+
+// Tests of `clone()` method implementations within JSON values
+module json_values_clone_unit_test;
+  import svunit_pkg::svunit_testcase;
+  import json_pkg::*;
+
+  string name = "json_values_clone_ut";
+  svunit_testcase svunit_ut;
+
+  function void build();
+    svunit_ut = new(name);
+  endfunction
+
+  task setup();
+    svunit_ut.setup();
+  endtask
+
+  task teardown();
+    svunit_ut.teardown();
+  endtask
+
+  `SVUNIT_TESTS_BEGIN
+
+  `SVTEST(dummy_test)
+  begin
+  end
+  `SVTEST_END
+
+  `SVUNIT_TESTS_END
+
+endmodule : json_values_clone_unit_test
