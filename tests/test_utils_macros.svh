0bf0f93e1c39edfc0309800775babd382f4bfe03
esynr3z <esynr3z@gmail.com>
2024-03-29 22:43:27 +0300

    test: add more tests from json parsing test suite

--- tests/test_utils_macros.svh
+++ tests/test_utils_macros.svh
@@ -30,5 +30,14 @@
     `FAIL_IF_LOG(err_msg.len() > 0, {"\n", err_msg, "\n"})\
   end
 
+// Automation macro to hide boilerplate of checking JSON file to be parsed with error.
+// If `GOLDEN_ERR` is provided, then parsed error is compared with the golden error.
+// Otherwise, only parsing status (err/ok) is evaluated.
+`define EXPECT_ERR_LOAD_FILE(PATH, GOLDEN_ERR=null)\
+  begin \
+    string err_msg = test_utils_pkg::expect_err_load_file(PATH, GOLDEN_ERR);\
+    `FAIL_IF_LOG(err_msg.len() > 0, {"\n", err_msg, "\n"})\
+  end
+
 `define TEST_UTILS_MACROS_SVH
 `endif // !TEST_UTILS_MACROS_SVH
