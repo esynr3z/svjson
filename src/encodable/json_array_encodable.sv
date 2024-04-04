d52f49648ac5bfbde316631d93c584ac48685fd5
esynr3z <esynr3z@gmail.com>
2024-04-04 08:44:01 +0300

    style: fix comment

--- src/encodable/json_array_encodable.sv
+++ src/encodable/json_array_encodable.sv
@@ -2,6 +2,6 @@
 interface class json_array_encodable extends json_value_encodable;
   typedef json_value_encodable values_t[$];
 
-  // Get queue with values of this JSON object
+  // Get queue with values of this JSON array
   pure virtual function values_t get_values();
 endclass : json_array_encodable
